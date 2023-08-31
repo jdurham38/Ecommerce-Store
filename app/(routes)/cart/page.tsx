4a12945772244cb3c0f3f7b2855cfd24c92312e1
Joshua Durham <jdurham9725@eagle.fgcu.edu>
2023-08-30 20:34:36 -0400

    fixed post checkout navbar bug

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,47 +1,17 @@
-"use client";
-
-import { useEffect, useState } from 'react';
-
 import Container from '@/components/ui/container';
-import useCart from '@/hooks/use-cart';
-
-import Summary from './components/summary'
-import CartItem from './components/cart-item';
+import CartPageClient from '@/components/ui/cart-client'; // Import the client-side component
 
 export const revalidate = 0;
 
 const CartPage = () => {
-  const [isMounted, setIsMounted] = useState(false);
-  const cart = useCart();
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) {
-    return null;
-  }
-
   return (
-    <div className="bg-white">
-      <Container>
-        <div className="px-4 py-16 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
-          <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
-            <div className="lg:col-span-7">
-              {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
-              <ul>
-                {cart.items.map((item) => (
-                  <CartItem key={item.id} data={item} />
-                ))}
-              </ul>
-            </div>
-            <Summary />
-          </div>
-        </div>
-      </Container>
-    </div>
-  )
+    <Container>
+      <section>
+        <h1 className="text-3xl font-semibold flex justify-center items-center pb-20 pt-10">Shopping Cart</h1>
+      </section>
+      <CartPageClient /> {/* Include the client-side component */}
+    </Container>
+  );
 };
 
 export default CartPage;
